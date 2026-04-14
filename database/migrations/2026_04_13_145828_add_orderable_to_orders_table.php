f5b9952bce017c5bcf37558d70e5028ae6dbc1df
WildCode <m.sadakhanov@skoleom.com>
2026-04-14 11:54:25 +0200

    .

--- database/migrations/2026_04_13_145828_add_orderable_to_orders_table.php
+++ database/migrations/2026_04_13_145828_add_orderable_to_orders_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->nullableMorphs('orderable');
+            $table->morphs('orderable');
         });
     }
 
